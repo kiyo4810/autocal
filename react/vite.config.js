a720d69ac2dfa1a051a6a9e3d8d7d8363c02c98e
kiyo4810 <kiyo4810@gmail.com>
2026-01-16 00:43:58 +0900

    vite 設定

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// react/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/autocal/react/dist/', // あなたのリポジトリ名に合わせて調整
+});
