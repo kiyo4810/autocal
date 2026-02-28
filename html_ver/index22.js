1a82dc24a5c51f44f3e067d1b8da2fe35e502e7a
kiyo4810 <kiyo4810@gmail.com>
2026-02-28 22:37:59 +0900

    index22 正規表現の練習2

--- html_ver/index22.js
+++ html_ver/index22.js
@@ -1,9 +1,18 @@
 // const regex = new RegExp('[^0-9]', 'g');
-const regex = /[0-9]$/g;
+const regex = /[0-9]*/g;
 console.log(regex);
 const nums = '01234567890';
 const result = nums.match(regex);
 console.log(result);
 
-const div = document.getElementById('main');
+const div = document.getElementById('main1');
 div.innerText = result;
+
+const seikihyogen = new RegExp('[a-z]+');
+result2 = seikihyogen.test('abc');
+const div2 = document.getElementById('main2');
+div2.innerText = result2;
+
+result3 = seikihyogen.test('123');
+const div3 = document.getElementById('main3');
+div3.innerText = result3;
